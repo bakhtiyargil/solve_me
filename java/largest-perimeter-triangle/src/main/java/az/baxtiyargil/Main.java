d065c0981d41c8835da8d4cc1a04882065a7e92f
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-09-28 13:34:43 +0400

    largest perimeter triangle

---
+++ java/largest-perimeter-triangle/src/main/java/az/baxtiyargil/Main.java
@@ -0,0 +1,28 @@
+package az.baxtiyargil;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int per = largestPerimeter(new int[]{2, 1, 2});
+        System.out.println(per);
+    }
+
+    public static int largestPerimeter(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        for (int lastIndex = nums.length - 1; lastIndex > 0; lastIndex--) {
+            if (nums[lastIndex] < nums[lastIndex - 1] + nums[lastIndex - 2]) {
+                return nums[lastIndex] + nums[lastIndex - 1] + nums[lastIndex - 2];
+            }
+            if (lastIndex - 2 == 0) {
+                break;
+            }
+        }
+        return 0;
+    }
+}
+
