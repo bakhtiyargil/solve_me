1615e4c3c0c9253749b9c4671eda8a292c08a1de
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-09-28 13:57:49 +0400

    main package name corrected

--- java/solution/src/main/java/az/baxtiyargil/solution/Main.java
+++ java/solution/src/main/java/az/baxtiyargil/solution/Main.java
@@ -1,4 +1,4 @@
-package az.baxtiyargil;
+package az.baxtiyargil.solution;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
