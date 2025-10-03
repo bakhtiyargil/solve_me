44af282384fc65c7588b02abef2258c7704f7296
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-10-03 18:38:05 +0400

    polygon triangulation partial

--- java/solution/src/main/java/az/baxtiyargil/solution/triangulationpolygon/TriangulationPolygon.java
+++ java/solution/src/main/java/az/baxtiyargil/solution/triangulationpolygon/TriangulationPolygon.java
@@ -1,56 +1,61 @@
 package az.baxtiyargil.solution.triangulationpolygon;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TriangulationPolygon {
 
-    int firstProduct = 1;
-    int secondProduct = 1;
-
     public int minScoreTriangulation(int[] values) {
+        var min = minScoreTriangulation2(values);
+        int[] shiftedValues = values.clone();
+        for (int i = 0; i < values.length - 1; i++) {
+            shiftedValues = shift(shiftedValues);
+            var newMin = minScoreTriangulation2(shiftedValues);
+            min = Math.min(min, newMin);
+        }
+        return min;
+    }
+
+    private int minScoreTriangulation2(int[] values) {
+        int localSum = 0;
+        var partialPolygon = new ArrayList<Integer>();
         int arrLength = values.length;
         if (arrLength < 3) {
-            return firstProduct;
+            return 0;
         }
         if (arrLength == 3) {
-            for (int v : values) {
-                firstProduct = v * firstProduct;
-            }
-            return firstProduct;
+            return localSum + (values[0] * values[1] * values[2]);
         }
-
-        if (arrLength % 2 == 0) {
-            for (int i = 0; i < arrLength - 1; ) {
-                if (i == arrLength - 2) {
-                    firstProduct = values[i] * values[arrLength - 1] * firstProduct;
-                } else {
-                    firstProduct = values[i] * firstProduct;
+        outer:
+        for (int i = 0; i < arrLength - 1; ) {
+            partialPolygon.add(values[i]);
+            localSum = localSum + (values[i] * values[i + 1] * values[i + 2]);
+            i += 2;
+            if (i == arrLength - 1 || i == arrLength - 2) {
+                partialPolygon.add(values[i]);
+                if (arrLength == 4 || ((partialPolygon.size() + 1 == 4) && (arrLength != 5))) {
+                    partialPolygon.add(values[arrLength - 1]);
                 }
-                i += 2;
-            }
-        } else {
-            for (int i = 0; i < arrLength - 1; ) {
-                firstProduct = values[i] * firstProduct;
-                i += 2;
+                localSum = localSum + minScoreTriangulation2(
+                        partialPolygon.stream().mapToInt(Integer::intValue).toArray()
+                );
+                break outer;
             }
         }
-        firstProduct = firstProduct + (values[0] * values[1] * values[2]);
+        return localSum;
+    }
 
-        if (arrLength % 2 == 0) {
-            for (int i = 1; i <= arrLength + 1; ) {
-                if (i == arrLength+1) {
-                    secondProduct = values[0] * values[arrLength - 1] * secondProduct;
-                } else {
-                    secondProduct = values[i] * secondProduct;
-                }
-                i += 2;
-            }
-        } else {
-            for (int i = 0; i < arrLength - 1; ) {
-                secondProduct = values[i] * secondProduct;
-                i += 2;
-            }
-        }
-        secondProduct = secondProduct + (values[1] * values[2] * values[3]);
+    private int[] shift(int[] values) {
+        //box int to Integer
+        List<Integer> shifted = Arrays.stream(values)
+                .boxed()
+                .collect(Collectors.toList());
+        //shift the first element to the end of the list
+        shifted.add(shifted.removeFirst());
 
-        return Math.min(firstProduct, secondProduct);
+        //unbox Integer to int
+        return shifted.stream().mapToInt(Integer::intValue).toArray();
     }
 }
--- java/solution/src/main/java/az/baxtiyargil/solution/triangulationpolygon/TriangulationPolygonMain.java
+++ java/solution/src/main/java/az/baxtiyargil/solution/triangulationpolygon/TriangulationPolygonMain.java
@@ -4,21 +4,53 @@ public class TriangulationPolygonMain {
 
     public static void main(String[] args) {
         var triangPoly = new TriangulationPolygon();
+        test1(triangPoly);
+        test2(triangPoly);
         test3(triangPoly);
+        test4(triangPoly);
+        test5(triangPoly);
+        test6(triangPoly);
+        test7(triangPoly);
+        test8(triangPoly);
     }
 
     private static void test1(TriangulationPolygon triangPoly) {
-        var sum = triangPoly.minScoreTriangulation(new int[]{3, 7, 4, 5});
-        System.out.println(sum);
+        var sum = triangPoly.minScoreTriangulation(new int[]{1, 2, 3});
+        System.out.println("Test 1: " + sum);
     }
 
     private static void test2(TriangulationPolygon triangPoly) {
-        var sum = triangPoly.minScoreTriangulation(new int[]{1, 2, 3});
-        System.out.println(sum);
+        var sum = triangPoly.minScoreTriangulation(new int[]{3, 7, 4, 5});
+        System.out.println("Test 2: " + sum);
     }
 
     private static void test3(TriangulationPolygon triangPoly) {
         var sum = triangPoly.minScoreTriangulation(new int[]{1, 3, 1, 4, 1, 5});
+        System.out.println("Test 3: " + sum);
+    }
+
+    private static void test4(TriangulationPolygon triangPoly) {
+        var sum = triangPoly.minScoreTriangulation(new int[]{3, 1, 2, 4, 6, 7, 8});
+        System.out.println(sum);
+    }
+
+    private static void test5(TriangulationPolygon triangPoly) {
+        var sum = triangPoly.minScoreTriangulation(new int[]{4, 3, 4, 3, 5});
+        System.out.println(sum);
+    }
+
+    private static void test6(TriangulationPolygon triangPoly) {
+        var sum = triangPoly.minScoreTriangulation(new int[]{2, 2, 2, 2, 1});
+        System.out.println(sum);
+    }
+
+    private static void test7(TriangulationPolygon triangPoly) {
+        var sum = triangPoly.minScoreTriangulation(new int[]{3, 1, 4, 5, 4});
+        System.out.println(sum);
+    }
+
+    private static void test8(TriangulationPolygon triangPoly) {
+        var sum = triangPoly.minScoreTriangulation(new int[]{5, 5, 5, 4, 2, 5});
         System.out.println(sum);
     }
 }
