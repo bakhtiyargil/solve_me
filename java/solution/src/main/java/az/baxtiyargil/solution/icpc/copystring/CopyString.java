dc98b3b6462e684b689ba80c4b73ead15fe5ef0c
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-11-14 17:22:02 +0400

    copy string partially complete

--- java/solution/src/main/java/az/baxtiyargil/solution/icpc/copystring/CopyString.java
+++ java/solution/src/main/java/az/baxtiyargil/solution/icpc/copystring/CopyString.java
@@ -9,23 +9,16 @@ public class CopyString {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-//        int testCases = scanner.nextInt();
-//        for (int t = 0; t < testCases; t++) {
-//            scanner.nextInt();
-//            int op = scanner.nextInt();
-//            scanner.nextLine();
-//            String rawString = scanner.nextLine();
-//            String idealString = scanner.nextLine();
-
-        constructAndPrint("abcd", "aabd", 1);
-        constructAndPrint("ab", "ab", 2);
-        constructAndPrint("abcde", "abbcc", 3);
-        constructAndPrint("egcnyeluw", "eegccyelw", 1);
-        constructAndPrint("vzvylxxmsy", "vvvvvllxxx", 3);
-        constructAndPrint("acba", "aaac", 6);
-        constructAndPrint("acabb", "aaaca", 7);
+        int testCases = scanner.nextInt();
+        for (int t = 0; t < testCases; t++) {
+            scanner.nextInt();
+            int op = scanner.nextInt();
+            scanner.nextLine();
+            String rawString = scanner.nextLine();
+            String idealString = scanner.nextLine();
+            constructAndPrint(rawString, idealString, op);
+        }
     }
-
 
     private static void constructAndPrint(String rawString, String idealString, int maxOp) {
         if (rawString.equals(idealString)) {
@@ -48,7 +41,8 @@ public class CopyString {
 
             StringBuilder alteredSb = new StringBuilder(rawSb.toString());
             for (int j = 1; j <= rawString.length() - 1; j++) {
-                if (alteredSb.toString().charAt(j) != idealString.charAt(j)) {
+                if ((alteredSb.toString().charAt(j) != idealString.charAt(j)) ||
+                        (j + 1 < idealString.length() && alteredSb.charAt(j - 1) == idealString.charAt(j + 1))) {
                     rawSb.setCharAt(j, alteredSb.charAt(j - 1));
                 }
             }
@@ -60,5 +54,15 @@ public class CopyString {
             System.out.println(i);
             results.forEach(System.out::println);
         }
+    }
+
+    private static void tests() {
+        constructAndPrint("abcd", "aabd", 1);
+        constructAndPrint("ab", "ab", 2);
+        constructAndPrint("abcde", "abbcc", 3);
+        constructAndPrint("egcnyeluw", "eegccyelw", 1);
+        constructAndPrint("vzvylxxmsy", "vvvvvllxxx", 3);
+        constructAndPrint("acba", "aaac", 6);
+        constructAndPrint("acabb", "aaaca", 7);
     }
 }
