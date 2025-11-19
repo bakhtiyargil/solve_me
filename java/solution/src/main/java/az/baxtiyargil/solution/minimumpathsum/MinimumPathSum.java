64f8f3c9abf0798a3aaea354e84cef179518f39f
Bakhtiyar V. Karimov <bext97@mail.ru>
2025-11-19 23:02:25 +0400

    Add minimum path sum

--- java/solution/src/main/java/az/baxtiyargil/solution/minimumpathsum/MinimumPathSum.java
+++ java/solution/src/main/java/az/baxtiyargil/solution/minimumpathsum/MinimumPathSum.java
@@ -4,35 +4,36 @@ public class MinimumPathSum {
 
     private static final int[][] arr = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
     private static final int[][] arr1 = new int[][]{{1, 2, 3}, {4, 5, 6}};
+    private static final int[][] arr2 = new int[][]{{0, 0}, {0, 0}};
+    private static final int[][] arr3 = new int[][]{{1, 2}, {1, 1}};
 
     public static void main(String[] args) {
         test();
     }
 
-    private static int minPathSum(int i, int j, int[][] arr) {
-        int next1 = 0;
-        int next2 = 0;
-
-        if (i != 0 && j != 0) {
-            next1 = arr[i - 1][j];
-            next2 = arr[i][j - 1];
-        } else if (i == 0 && j == 0) {
+    private static int minPathSum(int rowIndex, int columnIndex, int[][] arr) {
+        if (rowIndex == 0 && columnIndex == 0) {
             return arr[0][0];
-        } else if (j == 0) {
-            next2 = arr[i - 1][j];
-        } else {
-            next1 = arr[i][j - 1];
         }
 
-        if (next2 > next1) {
-            return arr[i][j] + minPathSum(i - 1, j, arr);
-        } else {
-            return arr[i][j] + minPathSum(i, j - 1, arr);
+        int up = Integer.MAX_VALUE;
+        int left = Integer.MAX_VALUE;
+
+        if (rowIndex > 0) {
+            up = minPathSum(rowIndex - 1, columnIndex, arr);
         }
+
+        if (columnIndex > 0) {
+            left = minPathSum(rowIndex, columnIndex - 1, arr);
+        }
+
+        return arr[rowIndex][columnIndex] + Math.min(up, left);
     }
 
     private static void test() {
         System.out.println(minPathSum(arr.length - 1, arr[0].length - 1, arr));
         System.out.println(minPathSum(arr1.length - 1, arr1[0].length - 1, arr1));
+        System.out.println(minPathSum(arr2.length - 1, arr2[0].length - 1, arr2));
+        System.out.println(minPathSum(arr3.length - 1, arr3[0].length - 1, arr3));
     }
 }
